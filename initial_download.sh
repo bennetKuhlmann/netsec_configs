7dc220191d3fdbe0dc1c3eed81a46c27b72088d6
bennetKuhlmann <carlbennet.k@gmail.com>
2024-06-22 15:13:40 +0200

    small changes

--- initial_download.sh
+++ initial_download.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sudo -s
 
+
+
 apt update
 apt install -y wireshark
 ntpdate pool.ntp.org
@@ -8,7 +10,10 @@ ntpdate pool.ntp.org
 git clone https://www.github.com/bennetKuhlmann/netsec_configs
 
 # down the last (internet able interface)
-ifdown eth1
+#ifdown eth1
+# enable internet interface
+#iptables -A INPUT --in-interface -j ACCEPT
+#iptables -A OUTPUT --out-interface -j ACCEPT
 
 # tmux
 tmux
